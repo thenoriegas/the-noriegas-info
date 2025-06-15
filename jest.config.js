496d4adacc8cd997e1fdeb14d88c08f1bb05531b
The Noriegas <thenoriegas@mailinator.com>
2025-06-14 18:02:50 -0700

    Add synth

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  cache: false,
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
-  moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/tests/mocks/styleMock.js"
-  }
-}
